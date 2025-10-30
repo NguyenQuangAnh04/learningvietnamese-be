8044fbc30bf8641d58c77176a4ce74b9449459c2
Nguyen Quang Anh <nguyenquanganh07042004@gmail.com>
2025-10-30 22:42:25 +0700

    update

--- src/main/java/com/example/vietjapaneselearning/repository/LessonRepository.java
+++ src/main/java/com/example/vietjapaneselearning/repository/LessonRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
-    @Query("SELECT l FROM Lesson l WHERE (:title IS NULL OR l.title LIKE %:title%)  AND (:level IS NULL OR l.level  like %:level% ) ")
+    @Query("SELECT l FROM Lesson l WHERE (:title IS NULL OR l.title LIKE %:title% OR l.titleJa LIKE %:title%)  AND (:level IS NULL OR l.level  like %:level% ) ")
     Page<Lesson> findByTitleAndLevel(@Param("title") String title, @Param("level") String level, Pageable pageable);
     @Query("SELECT l from Lesson l where l.title = :title OR l.titleJa = :title")
     Optional<Lesson> findByTitle(@Param("title") String title);
